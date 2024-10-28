c212c14a737b75fc38d86abe4333fc780940095d
kyo <100336613+kioryzou@users.noreply.github.com>
2024-10-28 15:46:28 -0600

    Update Configuracion.java
    Adicion de atributos y metodos sin la logica, de la clase configuracion

--- src/main/java/com/mycompany/proyectoestructura/Configuracion.java
+++ src/main/java/com/mycompany/proyectoestructura/Configuracion.java
@@ -1,1 +1,21 @@
 
+
+public class Configuracion {
+  private String NombreBanco;
+  private int CantidadCajas;
+  private int CajaPreferencial;
+  private int CajaRapdida;
+  
+  //------------------------Metodos------------------------//
+
+  public void cargarConfiguracion() {}
+
+  public void guardarConfiguracion() {}
+
+  
+    
+
+
+
+  
+}

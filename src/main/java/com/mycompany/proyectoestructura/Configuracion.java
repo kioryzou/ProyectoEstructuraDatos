cb9a751bd7f351bc1585ea6077a4afadc53f84f9
kyo <100336613+kioryzou@users.noreply.github.com>
2024-10-29 17:46:20 -0600

    Update Configuracion.java

--- src/main/java/com/mycompany/proyectoestructura/Configuracion.java
+++ src/main/java/com/mycompany/proyectoestructura/Configuracion.java
@@ -1,4 +1,8 @@
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Configuracion {
   private String NombreBanco;
@@ -9,19 +13,25 @@ public class Configuracion {
   //------------------------Metodos------------------------//
 
   public void cargarConfiguracion() {
-   File archivo = new File("prod.txt");
+   File CONF_FILE = new File("CONFIG.txt");
     //trabajando en los metodos al terminar actualizo en github
-    if (archivo.exists()) {
-    try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+    if (CONF_FILE.exists()) {
+            
+            try (BufferedReader br = new BufferedReader(new FileReader(CONF_FILE))) {
+                
                 NombreBanco = br.readLine();
                 CantidadCajas = Integer.parseInt(br.readLine());
                 CajaPreferencial = Integer.parseInt(br.readLine());
                 CajaRapida = Integer.parseInt(br.readLine());
-            } catch (IOException e) {
+                
+            } catch (IOException e) 
+            
+            {
                 System.out.println("Error al leer la configuración: " + e.getMessage());
             }
+            
+            
         }
-  }
 
   public void guardarConfiguracion() {}
 

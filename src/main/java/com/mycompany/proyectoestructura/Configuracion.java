ea91755a5c03bf4697435e936f70a48882f7ee2b
kyo <100336613+kioryzou@users.noreply.github.com>
2024-10-28 18:05:03 -0600

    Update Configuracion.java

--- src/main/java/com/mycompany/proyectoestructura/Configuracion.java
+++ src/main/java/com/mycompany/proyectoestructura/Configuracion.java
@@ -11,6 +11,16 @@ public class Configuracion {
   public void cargarConfiguracion() {
    File archivo = new File("prod.txt");
     //trabajando en los metodos al terminar actualizo en github
+    if (archivo.exists()) {
+    try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+                NombreBanco = br.readLine();
+                CantidadCajas = Integer.parseInt(br.readLine());
+                CajaPreferencial = Integer.parseInt(br.readLine());
+                CajaRapida = Integer.parseInt(br.readLine());
+            } catch (IOException e) {
+                System.out.println("Error al leer la configuración: " + e.getMessage());
+            }
+        }
   }
 
   public void guardarConfiguracion() {}
